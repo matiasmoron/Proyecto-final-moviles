70724c0f02a0f41e2802c3d04461d422c31043e9
Matias <matiasmoron008@gmail.com>
2015-11-27 04:20:32 -0600

    1er avance
    -Avance en la UI
    -Brujula
    -Audio

---
+++ AppFinal/www/js/audio.js
@@ -0,0 +1,26 @@
+function captureAudio() {
+	navigator.device.capture.captureAudio(captureAudioSuccess, captureError, {
+		limit: 1
+	});
+}
+
+function captureAudioSuccess(mediaFiles) {
+	var i, len;
+	var formatSuccess = function (mediaFile) {
+		//document.getElementById('format-data').innerHTML = "Duration: <strong>" + mediaFile.duration / 1000 + "s</strong><br/>";
+	};
+	for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+		document.getElementById('capture-result').innerHTML = "<strong>" + (i + 1) + "files</strong>";
+		mediaFiles[i].getFormatData(formatSuccess, formatError);
+	}
+}
+
+function captureError(error) {
+	var msg = 'A ocurrido un error durante la grabación, vuelva a intentarlo: ' + error.code;
+	navigator.notification.alert(msg, null, 'Error!');
+	document.getElementById('capture-result').innerHTML = "<strong>Error</strong>";
+}
+
+function formatError(error) {
+	alert("Error getting file format data: " + error.code);
+}
